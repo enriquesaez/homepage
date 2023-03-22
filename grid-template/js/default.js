cec4735339359c87fe67ddbdfea85fe86039dbc5
enriquesaez <saez240@gmail.com>
2023-03-22 11:24:20 +0100

    added js file, gpt html file and some css styles

---
+++ grid-template/js/default.js
@@ -0,0 +1,9 @@
+// Mobile menu //
+	function openNav() {
+		document.getElementById("mobileNav").style.height = "100%";
+	}
+
+	function closeNav() {
+		document.getElementById("mobileNav").style.height = "0%";
+	}
+// End Mobile menu //
